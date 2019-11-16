901871aec8da8c4090aba00dae1286a0fb28be6b
iibrahimli <imranibrahimli98@gmail.com>
2019-11-16 21:46:03 +0400

    add centrality metrics

--- pw2/util.py
+++ pw2/util.py
@@ -52,3 +52,49 @@ def path_cost(G, path, attr):
     """
 
     return sum([G.edges[n1, n2][attr] for n1, n2 in zip(path[:-1], path[1:])])
+
+
+def degree_of_connectivity(G, v):
+    """
+    Compute the degree of connectivity of a vertex v
+    """
+    if isinstance(G, nx.Graph):
+        return len(G.edges(v))
+    elif isinstance(G, nx.DiGraph):
+        return len(G.in_edges(v)) + len(G.out_edges(v))
+    else:
+        raise TypeError("check type of G")
+
+
+def shortest_paths(G, v, attr=None):
+    """
+    Get list of shortest paths from v to all other nodes
+    """
+    sp = []
+    for node in G.nodes:
+        if node == v: continue
+        sp.append(nx.shortest_path(G, source=v, target=node, weight=attr))
+    return sp
+
+
+def closeness_centrality(G, v, attr=None):
+    """
+    Compute the closeness centrality of a vertex v
+    """
+    sp = shortest_paths(G, v, attr=attr)
+    return sum(map(len, sp)) / len(sp)
+
+
+def betweenness_centrality(G, v, attr=None):
+    """
+    Compute the betweenness centrality of a vertex v
+    """
+    if isinstance(G, nx.Graph):
+        for 
+        sp = shortest_paths(G, v, attr=attr)
+        return len(G.edges(v))
+    elif isinstance(G, nx.DiGraph):
+        sp = shortest_paths(G, v, attr=attr)
+        return len(G.in_edges(v)) + len(G.out_edges(v))
+    else:
+        raise TypeError("check type of G")
