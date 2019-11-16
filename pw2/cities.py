0ce7abd94c0ec348881d9f92b316ef0e49df4cb9
iibrahimli <imranibrahimli98@gmail.com>
2019-11-16 21:45:54 +0400

    finish cities.py

--- pw2/cities.py
+++ pw2/cities.py
@@ -10,7 +10,20 @@ data = pd.read_csv("cities_in_az.csv")
 G = nx.from_pandas_edgelist(data, source='Origin', target='Destiny',
                             edge_attr=True, create_using=nx.DiGraph)
 
-# add 'visited' attribute to each node
-attribute_for_nodes(G, 'visited', False)
+print("shortest path Baku -> Imishli")
+print(" * without weights:", nx.shortest_path(G, source='Baku', target='Imishli'))
+print(" * with weights:", nx.shortest_path(G, source='Baku', target='Imishli', weight='Hours'))
+# 3.1.3: the total weight for path Baku -> Shamakhi -> Imishli is higher that the other,
+#        while the number of nodes needed to travel is lower
 
-print(nx.shortest_path(G, source='Baku', target='Kurdamir', weight='Hours'))
+
+nx.add_path(G, ['Baku', 'Imishli'])
+G.edges['Baku', 'Imishli']['Hours'] = 1.29
+
+print()
+print("shortest path Baku -> Imishli")
+print(" *", nx.shortest_path(G, source='Baku', target='Imishli', weight='Hours'))
+print("shortest path Imishli -> Baku")
+print(" *", nx.shortest_path(G, source='Imishli', target='Baku', weight='Hours'))
+# 3.1.5: again, the weight for direct route from Baku to Imishli is lower than others,
+#        but from Imishli to Baku the total weight is less by going through 2 more vertices
